2fc934bfc809b3e20a993599a0e07fac6297acf4
IvanFestina <ivanfestina@gmail.com>
2022-06-26 22:34:10 +0300

    final refactoring

--- src/features/TodolistsList/todolists-reducer.ts
+++ src/features/TodolistsList/todolists-reducer.ts
@@ -2,7 +2,10 @@ import {todolistsAPI} from '../../api/todolists-api'
 import {RequestStatusType} from '../Application'
 import {appActions} from '../CommonActions/App'
 import {createAsyncThunk, createSlice, PayloadAction} from '@reduxjs/toolkit'
-import {handleAsyncServerAppError, handleAsyncServerNetworkError,} from '../../utils/error-utils'
+import {
+    handleAsyncServerAppError,
+    handleAsyncServerNetworkError,
+} from '../../utils/error-utils'
 import {TodolistType} from '../../api/types'
 import {ThunkError} from '../../utils/types'
 
@@ -18,7 +21,10 @@ const fetchTodolistsTC = createAsyncThunk<{ todolists: TodolistType[] }, undefin
         return handleAsyncServerNetworkError(error, thunkAPI)
     }
 })
-const removeTodolistTC = createAsyncThunk<{ id: string }, string, ThunkError>('todolists/removeTodolist', async (todolistId, {dispatch, rejectWithValue}) => {
+const removeTodolistTC = createAsyncThunk<{ id: string }, string, ThunkError>('todolists/removeTodolist', async (todolistId, {
+    dispatch,
+    rejectWithValue
+}) => {
     //изменим глобальный статус приложения, чтобы вверху полоса побежала
     dispatch(setAppStatus({status: 'loading'}))
     //изменим статус конкретного тудулиста, чтобы он мог задизеблить что надо
@@ -80,7 +86,11 @@ export const slice = createSlice({
     extraReducers: builder => {
         builder
             .addCase(fetchTodolistsTC.fulfilled, (state, action) => {
-                return action.payload.todolists.map((tl) => ({...tl, filter: 'all', entityStatus: 'idle'}))
+                return action.payload.todolists.map((tl) => ({
+                    ...tl,
+                    filter: 'all',
+                    entityStatus: 'idle'
+                }))
             })
             .addCase(removeTodolistTC.fulfilled, (state, action) => {
                 const index = state.findIndex(tl => tl.id === action.payload.id)
@@ -89,7 +99,11 @@ export const slice = createSlice({
                 }
             })
             .addCase(addTodolistTC.fulfilled, (state, action) => {
-                state.unshift({...action.payload.todolist, filter: 'all', entityStatus: 'idle'})
+                state.unshift({
+                    ...action.payload.todolist,
+                    filter: 'all',
+                    entityStatus: 'idle'
+                })
             })
             .addCase(changeTodolistTitleTC.fulfilled, (state, action) => {
                 const index = state.findIndex(tl => tl.id === action.payload.id)
